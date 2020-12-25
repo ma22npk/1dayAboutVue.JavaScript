a86c9adb3a7c308213f05aa2b3099988371f54e1
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-26 01:30:36 +0900

    Vueインスタンスの記述例

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        helloVal: 'Hello Vue.js'
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         helloVal: 'Hello Vue.js'
+//     }
+// });
+// new Vue({
+//     el: '#app'
+// });
+// const car = new Vue({
+//     el: '#car',
+//     data: {
+//         bodyType: 'ワゴン'
+//     }
+// });
+// car.bodyType = 'セダン'; //セダンを代入
+// console.log(car.bodyType); //ワゴンと表示
+
+// console.log(car.bodyType); //セダンと表示される変数carに代入する。
+new Vue({
+    el: '#app', //HTML要素の指定
+    data() {
+        return {
+            count: 0 //データを反映させる
+        };
+    },
+    //メソッドの定義
+    methods: {
+        increment() {
+            this.count++
+        }
     }
 });
