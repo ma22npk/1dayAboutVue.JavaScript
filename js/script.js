32aad018e6c413ca9ee2a0f944d069f6a8ca6198
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-26 11:09:10 +0900

    data関数を使った文字列の表示

--- js/script.js
+++ js/script.js
@@ -17,30 +17,40 @@
 // console.log(car.bodyType); //ワゴンと表示
 
 // console.log(car.bodyType); //セダンと表示される変数carに代入する。
-new Vue({
-    el: '#app', //HTML要素の指定
-    data() {
-        return {
-            count: 0 //データを反映させる
+// new Vue({
+//     el: '#app', //HTML要素の指定
+//     data() {
+//         return {
+//             count: 0 //データを反映させる
+//         };
+//     },
+//     //メソッドの定義
+//     methods: {
+//         increment() {
+//             this.count++
+//         }
+//     }
+// });
+// var value = '文字'; //変数valueに文字列を代入
+// var num = 1000; //変数numに数値を代入
+
+// const func = sunction() {
+//     //処理
+// };
+// func(); //処理の実行
+
+// const hello = 'hello'; //定数なので再代入不可能
+// hello = 'こんにちわ'; //エラーになる。
+
+// let goodMorning = 'Good morning'; //変数なので再代入荷脳
+// goodMorning = 'おはよう'; //問題なし
+
+//Vueクラスのインスタンス化
+const app = new Vue({
+    el: '#app',
+    data() { //前行にカンマ「,」を追加し、データ関数を定義する
+        return { //オブジェクトをreturnで返却する
+            text: 'hello Vue.js' //textプロパティに文字列を設定
         };
-    },
-    //メソッドの定義
-    methods: {
-        increment() {
-            this.count++
-        }
     }
 });
-var value = '文字'; //変数valueに文字列を代入
-var num = 1000; //変数numに数値を代入
-
-const func = sunction() {
-    //処理
-};
-func(); //処理の実行
-
-const hello = 'hello'; //定数なので再代入不可能
-hello = 'こんにちわ'; //エラーになる。
-
-let goodMorning = 'Good morning'; //変数なので再代入荷脳
-goodMorning = 'おはよう'; //問題なし
