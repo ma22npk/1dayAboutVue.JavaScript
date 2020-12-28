00229e90c47898a6f9851d44d42d9cafafc4c765
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-28 18:19:45 +0900

    イベントハンドリングを使ってテキストを変化させるアプリ完成

--- js/part3.js
+++ js/part3.js
@@ -1,8 +1,29 @@
-new Vue ({
-  el:'#app',
-  methods: {
-    changeMsg() {
-//処理を記述する
+// new Vue ({
+//   el:'#app',
+//   methods: {
+//     changeMsg() {
+// //処理を記述する
+//     }
+//     }
+// });
+// new Vue ({
+//   el:'#app',
+//   methods: {
+//     changeMsg() {
+// //処理を記述する
+//     }
+//     }
+// });
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      text: 'テキストは変更前です。'
+    };
+  },
+  methods:{
+    textChange(){
+      this.text = 'テキストが変更されました！！';
     }
-    }
+  }
 });
