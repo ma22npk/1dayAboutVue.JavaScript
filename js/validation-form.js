deff04b7eded0285423121ed44d9bda8a0e61d2a
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2021-01-02 17:57:16 +0900

    算出プロパティisValidadName作成開始

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,17 +1,17 @@
-const peaple = new Vue ({
-  el: '#app',
-  data:{
-      age : '10'
-  },
+// const peaple = new Vue ({
+//   el: '#app',
+//   data:{
+//       age : '10'
+//   },
 
-  // computed:{
-  //   check_age: function(){
-  //     return this.age >= 20;
-  //   }
-  // }
-  computed:{
-    check_age(){
-      return this.age >= 20;
-    }
-  }
-});
+//   // computed:{
+//   //   check_age: function(){
+//   //     return this.age >= 20;
+//   //   }
+//   // }
+//   computed:{
+//     check_age(){
+//       return this.age >= 20;
+//     }
+//   }
+// });
