14379be92d8d2ec47b1cb1bac03841e3cad5b102
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2021-01-02 17:59:43 +0900

    JSの実装

--- js/validation-form.js
+++ js/validation-form.js
@@ -15,3 +15,16 @@
 //     }
 //   }
 // });
+new Vue({
+  el: '#app',
+  data(){
+    return {
+      name: 'name'
+    };
+  },
+  computed:{
+    isInValidName(){
+      return this.name.length < 4;
+    }
+  }
+});
