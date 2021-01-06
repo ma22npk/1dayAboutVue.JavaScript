d672ed559dc6f72d8c7d4379e58fd79bf0a36870
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2021-01-06 17:48:44 +0900

    名前、メールアドレス、電話番号のバリデーションチェック

--- js/validation-form.js
+++ js/validation-form.js
@@ -19,12 +19,23 @@ new Vue({
   el: '#app',
   data(){
     return {
-      name: 'name'
+      name: 'name',
+      email: 'email@email.com',
+      tel:'12345678'
     };
   },
   computed:{
     isInValidName(){
       return this.name.length < 4;
+    },
+    isInValidEmail(){
+      const reg = new RegExp(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/);
+      return !reg.test(this.email);
+    },
+    isInValidTel(){
+      const tel = this.tel;
+      const isErr = tel.length < 8 || isNaN(Number(tel));
+      return isErr;
     }
   }
 });
