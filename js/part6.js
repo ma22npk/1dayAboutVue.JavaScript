31d26d2627277af5c9843990c28d5af8dd8d5282
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2021-01-07 22:36:06 +0900

    色々なキー修飾子の種類

---
+++ js/part6.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: '#app',
+  methods: {
+    onEnter() {
+      alert('Enterでアラート発生!')
+    },
+    onTab() {
+      alert('Tabでアラート発生!')
+    },
+    onDelete() {
+      alert('Deleteでアラート発生!')
+    },
+    onEsc() {
+      alert('Escでアラート発生!')
+    },
+    onSpace() {
+      alert('Spaceでアラート発生!')
+    },
+    onUp() {
+      alert('Upでアラート発生!')
+    },
+    onDown() {
+      alert('Downでアラート発生!')
+    },
+    onLeft() {
+      alert('Leftでアラート発生!')
+    },
+    onRight() {
+      alert('Rightでアラート発生!')
+    }
+  }
+})
